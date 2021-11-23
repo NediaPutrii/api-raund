cc92c6084c1f4d8c62afad15c39e2f184a14e079
nediaputrii <50162029+NediaPutrii@users.noreply.github.com>
2021-11-24 02:29:20 +0700

    awal

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,18 @@
 */
 
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+    return ["Hello Hai..!!!"];
+});
+
+$router->get('/data', function () use ($router) {
+    $results = app('db')->select("SELECT * FROM barang");
+    return response()->json($results);
+});
+
+$router->post('/register', 'UserController@register');
+$router->post('/login','AuthController@login');
+
+
+$router->group(['middleware' => 'auth'], function() use ($router){
+    $router->post('/logout', 'AuthController@logout');
 });
